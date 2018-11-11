84abaffc2180651b89e92603353ed9b06962a68b
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2018-11-10 23:34:02 -1000

    spareChange ex number 8

--- exercises.js
+++ exercises.js
@@ -176,8 +176,14 @@ console.log(notEqual('hello', 'hello'))
  * The function will return true if the number passed into the function is greater than 100, otherwise it will return false.
  * Console.log your result.
 */ 
-
-
+function spareChange(money){
+  if(money > 100){
+    return true
+  }else{
+    return false
+  }
+}
+console.log(spareChange(200))
 
 /*
  * #9
