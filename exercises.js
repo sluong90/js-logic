23f216bb57ad3aa06a09440de414506b96fecb93
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2018-11-10 22:57:32 -1000

    isGreaterThan #3

--- exercises.js
+++ exercises.js
@@ -74,7 +74,15 @@ console.log(login('test1234'));
  * The function will return true if the first number is greater than the second.
  * Console.log your result.
 */
-
+function isGreaterThan(first, second){
+  if(first > second){
+    return true
+  }else{
+    return false
+  }
+  }
+  console.log(isGreaterThan(10,7));
+  
 
 
 /*
