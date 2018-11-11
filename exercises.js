53c0daa419df5aa22a07abe7d7235e41376e57a6
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2018-11-10 22:30:28 -1000

    logic #1

--- exercises.js
+++ exercises.js
@@ -33,7 +33,14 @@ var month = 'November';
  *
  * The function will return true if the number passed into the function is equal to or greater than Hawaii's voting age. Console.log your result.
 */
-
+function canVote(age){
+  if(age >= 18){
+    return true
+  }else{
+    return false
+  }
+}
+console.log('Can vote: ', canVote(7));
 
 /*
  * #2
