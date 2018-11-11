b01eb4a5928dfdc7c49c314495f63a6b905c5d1c
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2018-11-10 23:38:36 -1000

    dirty30 ex number 9

--- exercises.js
+++ exercises.js
@@ -198,8 +198,14 @@ console.log(spareChange(200))
  * The function will return true if the sum of all the number values is greater than 30, otherwise it will return false.
  * Console.log your result.
 */ 
-
-
+function dirty30(one, two, three){
+  if(one + two + three > 30){
+    return true
+  }else{
+    return false
+  }
+}
+console.log(dirty30(16,17,10))
 
 /*
  * #10
