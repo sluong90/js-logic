4f767996c56b64d47b71d94ff889f03b86a18016
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2018-11-10 23:24:03 -1000

    isEqual ex number 6

--- exercises.js
+++ exercises.js
@@ -135,7 +135,15 @@ console.log(bigBird('hey'))
  * If the strings are equal, the function will return the message "You look mahvelous!" Otherwise, return the message: "I don't know who you are anymore." 
  * Console.log your result.
 */
+function isEqual(first, second){
+  if (first === second){
+    return 'You look mahvelous!'
+  }else{
+    return 'I don\'t know who you are anymore.'
 
+  }
+}
+console.log(isEqual('hey', 'hey'))
 
 /*
  * #7
