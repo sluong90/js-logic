49febd97525f7b22f7cbe22e19f3f43588a63652
Sylvia L <sylvia@Sylvias-MacBook-Pro.local>
2018-11-12 21:45:10 -1000

    moneyTrain ex number 13

--- exercises.js
+++ exercises.js
@@ -274,8 +274,16 @@ console.log(graduation(100,false))
  * The function will return the message: "You are riding Honolulu's Rail.", if the number value is less than 50, otherwise it will return the message: "You are riding an Amtrak.", if the number value is less than 100, and return the message: "Now you ballin' in the Shinkansen!", if the number value is greater than or equal to 100.
  * Console.log your result.
 */ 
-
-
+function moneyTrain(speed){
+  if(speed < 50){
+    return 'You are riding Honolulu\'s Rail.'
+  }else if(speed < 100){
+    return 'You are riding an Amtrak.'
+  }else{
+    return "Now you ballin' in the Shinkansen!"
+  }
+}
+console.log(moneyTrain(200))
 /*
  * #14
  * Function - buyDoughnut
